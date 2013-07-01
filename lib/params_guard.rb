fbf212a4c158e2f12b63832415654a99a8b6ca4d
kswope <git-kevdev@snkmail.com>
2013-07-01 01:19:16 -0400

    ...

--- lib/params_guard.rb
+++ lib/params_guard.rb
@@ -5,39 +5,87 @@ class ParamsGuardException < Exception
 end
 
 
-class ParamsGuardParameters
 
-  def initialize(old_params)
-    @old_params = old_params
+class ParamsGuardParameters < ActionController::Parameters
+
+  def initialize(request_parameters, session)
+    @session = session
+    super(request_parameters)
   end
+
 
 
   def [](param, klass=nil)
 
-    return @old_params[param] unless klass # user wants original params
+    if klass
+      process_guard(klass, param)
+      return super(param)
+    else
+      return super(param)
+    end
+  end
+
+
+  private #~~*~~*~~*~~*~~*~~*~~*~~*~~*
+
+
+  def process_guard(klass, param)
+
+    model = Kernel.const_get(klass.to_s)
+    callback = "params_guard"
+    model.send(callback, param, @session) or
+    raise "#{klass}.#{callback} didn't return true"
 
   end
+
 
 end
 
 
 
-class ActionController::Base
+# class ParamsGuardParameters
+module ParamsGuard
 
-  # seems we need to use a before filters because params doesn't exist yet
-  # before_action :setup
-
-  alias old_params params
-
-  def params
-    ParamsGuardParameters.new(old_params)
-  end
-
-  # def setup
-  #   clog params.class
+  # def initialize(old_params)
+  #   @old_params = old_params
   # end
 
+
+  # def [](param, klass=nil)
+
+  #   return @old_params[param] unless klass # user wants original params
+
+  # end
+
+  # Returns a new ActionController::Parameters object that
+  # has been instantiated with the <tt>request.parameters</tt>.
+  def params
+    @_params ||= ParamsGuardParameters.new(request.parameters, request.session)
+  end
+
 end
+
+
+  ActionController::Base.send :include, ParamsGuard
+
+
+# class ActionController::Base
+
+#   # seems we need to use a before filters because params doesn't exist yet
+#   # before_action :setup
+
+
+#   alias old_params params
+
+#   def params
+#     ParamsGuardParameters.new(old_params)
+#   end
+
+#   # def setup
+#   #   clog params.class
+#   # end
+
+# end
 
 
 
