2c4357eeb89ff61c3418304e949a195714d682e5
kswope <git-kevdev@snkmail.com>
2013-07-03 20:25:19 -0400

    Removed old rails root from copied over project

--- test/rails_root/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-RailsRoot::Application.config.secret_token = '96bb4790c3b26a75e90b3a2f343722be2651b0d13237b05e2312ca72444333f594f0b1494316177ab2a719a6ee374d901f5fd33967d207ead5eaaec0fa6666f2'
