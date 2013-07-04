b16f449ce20bf4e897f2f61514b85771121514ba
kswope <git-kevdev@snkmail.com>
2013-07-03 20:37:53 -0400

    Created rails root for testing

---
+++ test/rails_root/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsRoot::Application.config.secret_key_base = '734c6254930fbc28f1db4a85f68a9ccc5aca33b87e774f8b8edab12641d5c14e2288ec0dd33bea1256d48659a316fbc102018c3f7907d06de07420128487e112'
---
+++ test/rails_root/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RailsRoot::Application.config.session_store :cookie_store, key: '_rails_root_session'
